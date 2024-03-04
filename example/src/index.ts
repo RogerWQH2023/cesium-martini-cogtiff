eec042bb34060b56631e9f6e3f1830e4ebe8d026
Roger WQH <879389031@qq.com>
2024-03-04 21:21:48 +0800

    feat: 增加样例（开发中）

--- example/src/index.ts
+++ example/src/index.ts
@@ -7,8 +7,13 @@ import {
   Math as CMath,
 } from "cesium";
 import "./index.css";
-import { MartiniTerrainProvider } from "@zjugis/cesium-martini";
+import {
+  MartiniTerrainProvider,
+  TiffMartiniTerrainProvider,
+} from "@zjugis/cesium-martini";
+
 import TIFFImageryProvider from "tiff-imagery-provider";
+import GeoTIFF, { Pool, fromUrl, fromBlob, GeoTIFFImage } from "geotiff";
 
 const viewer = new Viewer("cesiumContainer", {
   baseLayer: ImageryLayer.fromProviderAsync(
@@ -33,7 +38,7 @@ const viewer = new Viewer("cesiumContainer", {
   orderIndependentTranslucency: false,
 });
 
-const terrainLayer = new MartiniTerrainProvider({
+/* const terrainLayer = new MartiniTerrainProvider({
   url: new Resource({
     url: "https://api.mapbox.com/v4/mapbox.terrain-rgb/{z}/{x}/{y}@2x.webp",
     queryParameters: {
@@ -42,6 +47,9 @@ const terrainLayer = new MartiniTerrainProvider({
     },
   }),
   requestVertexNormals: true,
+}); */
+const terrainLayer = new TiffMartiniTerrainProvider({
+  url: "https://ddeassets-file.oss-cn-hongkong.aliyuncs.com/geophysics/global_map/gebco_dem.tif",
 });
 
 viewer.scene.terrainProvider = terrainLayer;
@@ -57,8 +65,44 @@ viewer.camera.setView({
 });
 
 const provider = await TIFFImageryProvider.fromUrl(
-  "https://ddeassets-file.oss-cn-hongkong.aliyuncs.com/geophysics/global_map/gebco_dem.tif"
-  //"https://tiff-imagery-provider.opendde.com/cogtif.tif"
+  //"https://ddeassets-file.oss-cn-hongkong.aliyuncs.com/geophysics/global_map/gebco_dem.tif"
+  "https://tiff-imagery-provider.opendde.com/cogtif.tif"
 );
 
 viewer.imageryLayers.addImageryProvider(provider);
+
+const resource = await fromUrl(
+  "https://ddeassets-file.oss-cn-hongkong.aliyuncs.com/geophysics/global_map/gebco_dem.tif"
+);
+console.log(resource);
+const image = await resource.getImage(0); // by default, the first image is read.
+
+const width = image.getWidth();
+const height = image.getHeight();
+const tileWidth = image.getTileWidth();
+const tileHeight = image.getTileHeight();
+const samplesPerPixel = image.getSamplesPerPixel();
+console.log(width);
+console.log(height);
+console.log(tileWidth);
+console.log(tileHeight);
+console.log(samplesPerPixel);
+
+// when we are actually dealing with geo-data the following methods return
+// meaningful results:
+const origin = image.getOrigin();
+const resolution = image.getResolution();
+const bbox = image.getBoundingBox();
+console.log(origin);
+console.log(resolution);
+console.log(bbox);
+
+//const data = await image.readRasters();
+//console.log(data);
+
+// 获取波段数
+const samples = image.getSamplesPerPixel();
+console.log(samples);
+// 获取nodata值
+const noData = image.getGDALNoData();
+console.log(noData);
