b5f7fac9c4d5588cc18d69809c700b7385f98178
Roger WQH <879389031@qq.com>
2024-03-04 10:11:53 +0800

    feat: 更新样例，测试加载tiff功能

--- example/src/index.ts
+++ example/src/index.ts
@@ -1,16 +1,30 @@
-import { ArcGisMapServerImageryProvider, Cartesian3, ImageryLayer, Resource, Viewer, Math as CMath } from 'cesium';
-import './index.css';
+import {
+  ArcGisMapServerImageryProvider,
+  Cartesian3,
+  ImageryLayer,
+  Resource,
+  Viewer,
+  Math as CMath,
+} from "cesium";
+import "./index.css";
 import { MartiniTerrainProvider } from "@zjugis/cesium-martini";
+import TIFFImageryProvider from "tiff-imagery-provider";
 
-const viewer = new Viewer('cesiumContainer', {
-  baseLayer: ImageryLayer.fromProviderAsync(ArcGisMapServerImageryProvider.fromUrl('https://services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer', {
-    enablePickFeatures: false
-  }), {}),
+const viewer = new Viewer("cesiumContainer", {
+  baseLayer: ImageryLayer.fromProviderAsync(
+    ArcGisMapServerImageryProvider.fromUrl(
+      "https://services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer",
+      {
+        enablePickFeatures: false,
+      }
+    ),
+    {}
+  ),
   baseLayerPicker: false,
   animation: false,
   fullscreenButton: false,
   geocoder: false,
-  homeButton: false,
+  //homeButton: false,
   selectionIndicator: true,
   timeline: false,
   navigationHelpButton: false,
@@ -21,9 +35,10 @@ const viewer = new Viewer('cesiumContainer', {
 
 const terrainLayer = new MartiniTerrainProvider({
   url: new Resource({
-    url: 'https://api.mapbox.com/v4/mapbox.terrain-rgb/{z}/{x}/{y}@2x.webp',
+    url: "https://api.mapbox.com/v4/mapbox.terrain-rgb/{z}/{x}/{y}@2x.webp",
     queryParameters: {
-      access_token: 'pk.eyJ1Ijoic3ZjLW9rdGEtbWFwYm94LXN0YWZmLWFjY2VzcyIsImEiOiJjbG5sMnFlZ28wb2d5MnFtb2xnMG90OW96In0.IE8Vqs0NTzCY0WqPzV9kcw'
+      access_token:
+        "pk.eyJ1Ijoic3ZjLW9rdGEtbWFwYm94LXN0YWZmLWFjY2VzcyIsImEiOiJjbG5sMnFlZ28wb2d5MnFtb2xnMG90OW96In0.IE8Vqs0NTzCY0WqPzV9kcw",
     },
   }),
   requestVertexNormals: true,
@@ -35,8 +50,15 @@ const extent = Cartesian3.fromDegrees(14.5481193, -21.433786, 8000);
 viewer.camera.setView({
   destination: extent,
   orientation: {
-    heading: CMath.toRadians(0), 
+    heading: CMath.toRadians(0),
     pitch: CMath.toRadians(-15),
     roll: 0.0,
   },
 });
+
+const provider = await TIFFImageryProvider.fromUrl(
+  "https://ddeassets-file.oss-cn-hongkong.aliyuncs.com/geophysics/global_map/gebco_dem.tif"
+  //"https://tiff-imagery-provider.opendde.com/cogtif.tif"
+);
+
+viewer.imageryLayers.addImageryProvider(provider);
